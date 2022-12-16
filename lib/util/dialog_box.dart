eba1e572d38aafff0c2b7de433cf3dcc03daded2
CCerrer <mathferrer17ea@gmail.com>
2022-12-16 09:23:57 -0300

    To Do app UI

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/util/buttons.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // getting input
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+
+            // buttons -> save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // save button
+                Buttons(text: "Save", onPressed: onSave),
+
+                const SizedBox(width: 10),
+
+                // cancel button
+                Buttons(text: "Cancel", onPressed: onCancel),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
